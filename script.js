15766f58559bb559bc4b05399e0532f175c81e2a
Jayasoorya <jayaspassion@gmail.com>
2021-06-19 18:31:42 +0530

    Intermediate

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.onload = () => {
-  const calculatorBody = document.createElement("div");
-  calculatorBody.setAttribute("class", "wrapper container");
+console.log("Hello World");
 
-  const innerElem = document.createElement("div");
-  innerElem.setAttribute("class", "col inner-elements");
+const calculatorBody = document.createElement("div");
+console.log("Body Created");
+calculatorBody.setAttribute("class", "wrapper container");
+const innerElem = document.createElement("div");
+innerElem.setAttribute("class", "col inner-elements");
+console.log("InnerElements Created");
 
-  innerElem.innerHTML = `
+innerElem.innerHTML = `
     <div class="row logo">calculator</div>
     <div class=" row display">
     <div class="previous-op"></div>
@@ -15,26 +17,26 @@ window.onload = () => {
         <div class="col"><button class="num-button">7</button></div>
         <div class="col"><button class="num-button">8</button></div>
         <div class="col"><button class="num-button">9</button></div>
-        <div class="col"><button class="op-button">-</button></div>
+        <div class="col"><button class="op-button sub">-</button></div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="num-button">4</button></div>
         <div class="col"><button class="num-button">5</button></div>
         <div class="col"><button class="num-button">6</button></div>
-        <div class="col"><button class="op-button">+</button></div>
+        <div class="col"><button class="op-button add">+</button></div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="num-button">1</button></div>
         <div class="col"><button class="num-button">2</button></div>
         <div class="col"><button class="num-button">3</button></div>
-        <div class="col"><button class="op-button">/</button></div>
+        <div class="col"><button class="op-button div">/</button></div>
     </div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="dot-button">.</button></div>
         <div class="col"><button class="num-button">0</button></div>
-        <div class="col"><button class="op-button">%</button></div>
-        <div class="col"><button class="op-button">*</button></div>
+        <div class="col"><button class="op-button mod">%</button></div>
+        <div class="col"><button class="op-button mul">X</button></div>
     </div>
     </div>
         <div class=" row calc-buttons">    
@@ -44,27 +46,62 @@ window.onload = () => {
     </div>
 
     `;
-  document.body.appendChild(calculatorBody);
-  calculatorBody.append(innerElem);
 
-  const numberButton = document.querySelectorAll(".num-button");
+//   document.body.appendChild(calculatorBody);
+//   calculatorBody.append(innerElem);
 
-  const disp = document.querySelector(".display");
-  const prevOp = document.querySelector(".previous-op");
-  const currentOp = document.querySelector(".current-op");
+document.body.appendChild(calculatorBody);
+calculatorBody.append(innerElem);
+console.log("Body and Elements appended");
 
-  numberButton.forEach((button) => {
-    button.onclick = function refreshDisp() {
-      currentOp.innerText = currentOp.innerText + button.innerText;
+const numberButton = document.querySelectorAll(".num-button");
+
+const disp = document.querySelector(".display");
+const prevOp = document.querySelector(".previous-op");
+const currentOp = document.querySelector(".current-op");
+
+numberButton.forEach((button) => {
+  button.onclick = function refreshDisp() {
+    currentOp.innerText = currentOp.innerText + button.innerText;
+  };
+});
+
+console.log(numberButton);
+
+const clearButton = document.querySelector(".clear-button");
+clearButton.onclick = "clear()";
+
+function clear() {
+  currentOp.innerText = "";
+  prevOp.innerText = "";
+}
+
+const opButton = document.querySelectorAll(".op-button");
+console.log(opButton);
+opButton.forEach((button) => {
+  const op = button.innerText;
+  console.log(op);
+  if (op == "+") {
+    button.onclick = () => {
+      console.log("Addition");
+      currentOp.innerText = "Addition";
     };
-  });
-
-  const clearButton = document.querySelector('.clear-button');
-  clearButton.onclick = () => {
-    disp.innerText = "";
+  } else if (op == "-") {
+    button.onclick = () => {
+      console.log("Subtraction");
+      currentOp.innerText = "Addition";
+    };
+  } else if (op == "/") {
+    button.onclick = () => {
+      currentOp.innerText = "Addition";
+    };
+  } else if (op == "%") {
+    button.onclick = () => {
+      currentOp.innerText = "Addition";
+    };
+  } else {
+    button.onclick = () => {
+      currentOp.innerText = "Addition";
+    };
   }
-
-  //   const opButton = document.querySelectorAll(".op-button");
-
-  
-};
+});
