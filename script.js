b677b4fe588738db4dd2952d05a8c5a0bde5678b
Jayasoorya <jayaspassion@gmail.com>
2021-07-02 18:00:41 +0530

    Functionality Added

--- script.js
+++ script.js
@@ -1,107 +1,138 @@
-console.log("Hello World");
+var state = {
+  previousOperand: "",
+  currentOperand: "",
+  operator: "",
+};
 
+//create calculator body
 const calculatorBody = document.createElement("div");
-console.log("Body Created");
 calculatorBody.setAttribute("class", "wrapper container");
 const innerElem = document.createElement("div");
 innerElem.setAttribute("class", "col inner-elements");
-console.log("InnerElements Created");
-
 innerElem.innerHTML = `
     <div class="row logo">calculator</div>
     <div class=" row display">
-    <div class="previous-op"></div>
-    <div class="current-op"></div>
+    <div class="previous-op">${state.previousOperand}</div>
+    <div class="current-op">${state.currentOperand}</div>
     </div>
     <div class=" row calc-buttons">    
         <div class="col"><button class="num-button">7</button></div>
         <div class="col"><button class="num-button">8</button></div>
         <div class="col"><button class="num-button">9</button></div>
-        <div class="col"><button class="op-button sub">-</button></div>
+        <div class="col"><button onclick="opSubButtonClicked()" class="op-button sub">-</button></div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="num-button">4</button></div>
         <div class="col"><button class="num-button">5</button></div>
         <div class="col"><button class="num-button">6</button></div>
-        <div class="col"><button class="op-button add">+</button></div>
+        <div class="col"><button onclick="opAddButtonClicked()" class="op-button add">+</button></div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="num-button">1</button></div>
         <div class="col"><button class="num-button">2</button></div>
         <div class="col"><button class="num-button">3</button></div>
-        <div class="col"><button class="op-button div">/</button></div>
+        <div class="col"><button onclick="opDivButtonClicked()" class="op-button div">/</button></div>
     </div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="dot-button">.</button></div>
         <div class="col"><button class="num-button">0</button></div>
-        <div class="col"><button class="op-button mod">%</button></div>
-        <div class="col"><button class="op-button mul">X</button></div>
+        <div class="col"><button onclick="opModButtonClicked()" class="op-button mod">%</button></div>
+        <div class="col"><button onclick="opMulButtonClicked()" class="op-button mul">X</button></div>
     </div>
     </div>
         <div class=" row calc-buttons">    
         <div class="col"><button class="delete-button"><-</button></div>
         <div class="col"><button class="clear-button">C</button></div>
-        <div class="col-6"><button class="result-button">=</button></div>
+        <div class="col-6"><button onClick="result()" class="result-button">=</button></div>
     </div>
 
     `;
 
-//   document.body.appendChild(calculatorBody);
-//   calculatorBody.append(innerElem);
-
 document.body.appendChild(calculatorBody);
 calculatorBody.append(innerElem);
-console.log("Body and Elements appended");
 
 const numberButton = document.querySelectorAll(".num-button");
-
 const disp = document.querySelector(".display");
 const prevOp = document.querySelector(".previous-op");
 const currentOp = document.querySelector(".current-op");
 
 numberButton.forEach((button) => {
   button.onclick = function refreshDisp() {
-    currentOp.innerText = currentOp.innerText + button.innerText;
+    state.currentOperand = state.currentOperand + button.innerText;
+    currentOp.innerText = state.currentOperand;
   };
 });
 
-console.log(numberButton);
-
 const clearButton = document.querySelector(".clear-button");
-clearButton.onclick = "clear()";
-
-function clear() {
+clearButton.onclick = () => {
   currentOp.innerText = "";
   prevOp.innerText = "";
+  state.currentOperand = "";
+  state.previousOperand = "";
+};
+
+function result() {
+  state.currentOperand = currentOp.innerText;
+  
+  let res;
+  switch (state.operator) {
+    case "+":
+        console.log(state.currentOperand, state.previousOperand, state.operator)
+      res = +state.previousOperand + +state.currentOperand;
+      console.group(res);
+      refreshDisp(res);
+      break;
+    case "-":
+      res = +state.previousOperand - +state.currentOperand;
+      refreshDisp(res);
+      break;
+    case "/":
+      res = +state.previousOperand / +state.currentOperand;
+      refreshDisp(res);
+      break;
+    case "*":
+      res = +state.previousOperand * +state.currentOperand;
+      refreshDisp(res);
+      break;
+    case "%":
+      res = +state.previousOperand % +state.currentOperand;
+      refreshDisp(res);
+      break;
+  }
 }
 
-const opButton = document.querySelectorAll(".op-button");
-console.log(opButton);
-opButton.forEach((button) => {
-  const op = button.innerText;
-  console.log(op);
-  if (op == "+") {
-    button.onclick = () => {
-      console.log("Addition");
-      currentOp.innerText = "Addition";
-    };
-  } else if (op == "-") {
-    button.onclick = () => {
-      console.log("Subtraction");
-      currentOp.innerText = "Addition";
-    };
-  } else if (op == "/") {
-    button.onclick = () => {
-      currentOp.innerText = "Addition";
-    };
-  } else if (op == "%") {
-    button.onclick = () => {
-      currentOp.innerText = "Addition";
-    };
-  } else {
-    button.onclick = () => {
-      currentOp.innerText = "Addition";
-    };
-  }
-});
+function refreshDisp(res) {
+    prevOp.innerText = prevOp.innerText + currentOp.innerText;
+  currentOp.innerText = res;
+}
+
+//functions for performing operations
+function opAddButtonClicked() {
+    console.log("addition")
+  operation("+");
+}
+
+function opSubButtonClicked() {
+  operation("-");
+}
+
+function opDivButtonClicked() {
+  operation("/");
+}
+
+function opMulButtonClicked() {
+  operation("*");
+}
+function opModButtonClicked() {
+  operation("%");
+}
+
+function operation(opr) {
+  state.operator = opr;
+  state.previousOperand = state.currentOperand;
+  console.log(state.previousOperand);
+  state.currentOperand = "";
+  prevOp.innerText = state.previousOperand + state.operator;
+  currentOp.innerText = "";
+}
